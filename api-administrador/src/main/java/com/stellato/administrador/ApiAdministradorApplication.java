a2eee6f10a6ec337f458cff9852b64eeb4bc8597
robertosxav <roberto.sxav@gmail.com>
2023-03-21 21:46:24 -0400

    commit administrador

---
+++ api-administrador/src/main/java/com/stellato/administrador/ApiAdministradorApplication.java
@@ -0,0 +1,13 @@
+package com.stellato.administrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdministradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdministradorApplication.class, args);
+	}
+
+}
