470b18fe323d20cdc50fcfcc57dbfac55e2be359
robertosxav <roberto.sxav@gmail.com>
2023-03-22 22:12:38 -0400

    correção deixar metodo update generico

--- api-vendas/src/main/java/com/stellato/vendas/domain/shared/repository/RepositoryInterface.java
+++ api-vendas/src/main/java/com/stellato/vendas/domain/shared/repository/RepositoryInterface.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.stellato.vendas.domain.lead.entity.LeadEntity;
-
 public interface RepositoryInterface<T> {
 	public T create(T entity);
-	public T update(Long id, LeadEntity leadEntityFront);
+	public T update(Long id, T entity);
 	public T findById(Long id);
 	public List<T> findAll();
 	public List<T> findAllActives();
