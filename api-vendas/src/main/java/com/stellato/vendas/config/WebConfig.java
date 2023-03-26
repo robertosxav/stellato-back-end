117ab2baf477a42f5f374d35595467d163f21a8f
robertosxav <roberto.sxav@gmail.com>
2023-03-26 10:27:25 -0400

    working progress; configurações cors

---
+++ api-vendas/src/main/java/com/stellato/vendas/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.stellato.vendas.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableConfigurationProperties(ConfigProperty.class)
+public class WebConfig implements WebMvcConfigurer {
+
+}
