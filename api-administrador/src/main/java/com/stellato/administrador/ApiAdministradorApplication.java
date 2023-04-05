2137dc582c24b03f7f1837b4c03b36aeb519da0a
robertosxav <roberto.sxav@gmail.com>
2023-04-04 21:54:09 -0400

    config swagger

--- api-administrador/src/main/java/com/stellato/administrador/ApiAdministradorApplication.java
+++ api-administrador/src/main/java/com/stellato/administrador/ApiAdministradorApplication.java
@@ -3,7 +3,10 @@ package com.stellato.administrador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ApiAdministradorApplication {
 
 	public static void main(String[] args) {
