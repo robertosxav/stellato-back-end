ce9ca5bf33c4dd1ca6c4023b747a491d0b6b2ced
robertosxav <roberto.sxav@gmail.com>
2023-06-09 10:04:55 -0400

    configuracoes cors

---
+++ api-stellato/src/main/java/com/stellato/config/ConfigProperty.java
@@ -0,0 +1,17 @@
+package com.stellato.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("config")
+public class ConfigProperty {
+	private String originPermitida = "http://localhost:8080";
+	
+	public String getOriginPermitida() {
+		return originPermitida;
+	}
+
+	public void setOriginPermitida(String originPermitida) {
+		this.originPermitida = originPermitida;
+	}
+
+}
