22a0ee6cdf309a3defdebac7838d6a882a29717b
robertosxav <roberto.sxav@gmail.com>
2023-06-17 16:52:57 -0400

    correcao repository material cotado

--- api-stellato/src/main/java/com/stellato/repository/MaterialCotadoRepository.java
+++ api-stellato/src/main/java/com/stellato/repository/MaterialCotadoRepository.java
@@ -1,10 +1,20 @@
 package com.stellato.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.stellato.model.MaterialCotado;
 
 @Repository
 public interface MaterialCotadoRepository extends JpaRepository<MaterialCotado, Long>{ 
+	@Query("SELECT mc FROM MaterialCotado mc WHERE mc.status =1")
+	List<MaterialCotado> listarTodosAtivos();
 	
+	@Query("SELECT mc FROM MaterialCotado mc WHERE mc.status =1")
+	Page<MaterialCotado> buscaGenerica(String pesquisa, Pageable pageable); 
 } 
